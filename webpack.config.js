44ea8bfc8d1d7dbedc87a3e5eb37dc6bcaffeafc
Felix Mariotto <felixmariotto@gmail.com>
2022-02-10 09:57:33 +0100

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ module.exports = env => {
     }, 
 
     devServer: {
-      contentBase: path.resolve(__dirname, 'dist'),
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
     },
 
     plugins: [
